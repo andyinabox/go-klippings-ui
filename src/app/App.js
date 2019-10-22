5b6a07acabd8b7cd4c24b0bbcc81fc375e3466bd
Andy Dayton <amdayton@gmail.com>
2019-10-21 23:08:38 -0500

    Set up testing suite

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Modal from "react-modal";
-import ReactDOM from "react-dom";
+// import Modal from "react-modal";
+// import ReactDOM from "react-dom";
 
-Modal.setAppElement("#app")
+// Modal.setAppElement("#app")
 
 function App() {
   return (
---
+++ src/app/App.test.js
@@ -0,0 +1,5 @@
+import App from './App'
+
+test('App should be a function', () => {
+  expect(typeof App).toBe('function');
+});
