bc21ee05b738819c9a3421b5dcb67e6fd33ae60d
Andy Dayton <amdayton@gmail.com>
2019-10-21 22:27:26 -0500

    Moving the furniture around

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,85 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ManifestPlugin = require('webpack-manifest-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin')
 const Dotenv = require("dotenv-webpack");
 
+const webpackEnv = process.env.WEBPACK_ENV
+const isEnvDevelopment = webpackEnv === 'development'
+const isEnvProduction = webpackEnv === 'production'
+
 module.exports = {
-  entry: "./app/index.js",
+  entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js"
+    filename: "bundle.js",
+    // publicPath: ""
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: "babel-loader", exclude: /node_modules/ },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
-      { test: /\.(png|jpe?g|gif)$/i, use: "file-loader" }
-    ]
+      // babel
+      {
+        test: /\.(js)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      },
+      // style and css extract
+      {
+        test: [/.css$|.scss$/],
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader", {
+          loader: 'postcss-loader',
+          options: {
+            plugins: () => [
+              require('autoprefixer')()
+            ],
+          }
+        }]
+      },
+      //image file loader
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/img/",
+              // publicPath: '../img/'
+            }
+          }
+        ]
+      },
+      //fonts
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'assets/fonts/',
+            // publicPath: '../fonts'
+          }
+        }]
+      }]
   },
-  mode: "development",
+  mode: webpackEnv,
+  devtool: 'source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./app/index.html"
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css"
     }),
     new Dotenv()
-  ]
+  ],
+  optimization: {
+    minimize: isEnvProduction,
+    minimizer: [
+      new TerserPlugin()
+    ],
+  }
 };
