3927a59b74248b2e82b1cc31729bc1d94f90ed7a
Andy Dayton <amdayton@gmail.com>
2019-10-21 23:31:50 -0500

    Setup dom testing

--- src/app/App.test.js
+++ src/app/App.test.js
@@ -1,5 +1,17 @@
+import React from 'react';
+import { cleanup, fireEvent, render } from '@testing-library/react';
 import App from './App'
+
+
+afterEach(cleanup);
 
 test('App should be a function', () => {
   expect(typeof App).toBe('function');
 });
+
+
+test("Let's try some DOM testing", () => {
+  const { getByText } = render(<App />)
+  console.log(getByText("Welcome to React!"))
+  expect(getByText("Welcome to React!")).toBeTruthy()
+})
